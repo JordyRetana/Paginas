2daaf030378cfb05f76bafcedc9397c979501959
Jordy Retana <jretanamendez@gmail.com>
2024-06-11 00:06:40 -0600

    Tarea Practica 2

---
+++ src/main/java/com/Paginas_Practica/PaginasPractica2Application.java
@@ -0,0 +1,13 @@
+package com.Paginas_Practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginasPractica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginasPractica2Application.class, args);
+	}
+
+}
